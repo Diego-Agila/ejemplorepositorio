9a4bf87bd3dc8d520c533512f52cea99e77eda7b
Diego-Agila <benlly.agila99@gmail.com>
2021-12-20 08:38:54 -0500

    Nuevo Repositorio

---
+++ src/cliente/clientes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+
+
+/**
+ *
+ * @author DIEGO
+ */
+public class clientes {
+    public static void main(String[] args) {
+        jframeCliente c = new jframeCliente();
+        c.setVisible(true);
+    }
+}
